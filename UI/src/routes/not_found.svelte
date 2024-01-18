eacb9e22e8ffebe3076bf461c0232bc7822cbd89
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-18 22:04:55 +0530

    not found page

--- UI/src/routes/not_found.svelte
+++ UI/src/routes/not_found.svelte
@@ -6,5 +6,5 @@
   class="fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center gap-[20px]"
 >
   <h1 class="text-3xl">The page you are looking for could not be found!</h1>
-  <a class="text-2xl underline" href="/" use:link>Go Home</a>
+  <a class="text-2xl link" href="/" use:link>Go Home</a>
 </div>
