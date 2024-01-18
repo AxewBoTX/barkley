f290ed363c5c517ac61e5f1f9c6a096e07508c53
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-18 13:46:26 +0530

    fiber server

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+
+	ui "dondu/UI"
+)
+
 func main() {
-	println("Hello, World!")
+	const PORT string = ":3000"
+	server := fiber.New(fiber.Config{
+		AppName: "Dondu",
+	})
+	server.Use("/", filesystem.New(filesystem.Config{
+		Root:       http.FS(ui.DistDir),
+		PathPrefix: "dist",
+		Browse:     true,
+	}))
+	server.Listen(PORT)
 }
