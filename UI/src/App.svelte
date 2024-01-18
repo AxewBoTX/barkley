7bd0e2cdf575ea742ca1d76e72c652fa110a2d43
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-18 21:39:42 +0530

    routes setup

--- UI/src/App.svelte
+++ UI/src/App.svelte
@@ -1,1 +1,18 @@
-<h1>Hello, World!</h1>
+<script>
+  import Router from "svelte-spa-router";
+  import Home from "./routes/home.svelte";
+  import About from "./routes/about.svelte";
+  import Posts from "./routes/posts.svelte";
+  import NotFound from "./routes/not_found.svelte";
+
+  const routes = {
+    "/": Home,
+    "/about": About,
+    "/posts": Posts,
+    "*": NotFound,
+  };
+</script>
+
+<body>
+  <Router {routes} />
+</body>
