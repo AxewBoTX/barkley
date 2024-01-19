cf7ae4d0091bfb733a0ab26b1db6265019de5943
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-19 13:29:22 +0530

    todo api routes

---
+++ API/todo.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Todo(router fiber.Router) {
+	router.Get("/todo", func(c *fiber.Ctx) error {
+		return c.JSON("Get Todo List")
+	})
+	router.Post("/todo", func(c *fiber.Ctx) error {
+		return c.JSON("Create Todo")
+	})
+	router.Patch("/todo/:todoID", func(c *fiber.Ctx) error {
+		return c.JSON("Update Todo with ID")
+	})
+	router.Delete("/todo/:todoID", func(c *fiber.Ctx) error {
+		return c.JSON("Delete Todo with ID")
+	})
+}
