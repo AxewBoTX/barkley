3c026e5e0fae7c5dacf07c1b7a8a465309132ca9
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-20 15:58:05 +0530

    Create Todo Modal

---
+++ UI/src/components/createTodoModal.svelte
@@ -0,0 +1,14 @@
+<dialog id="createTodoModal" class="modal">
+  <div class="modal-box">
+    <form method="dialog">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        >✕</button
+      >
+    </form>
+    <h3 class="font-bold text-lg">CreateModal</h3>
+    <p class="py-4">Press ESC key or click outside to close</p>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
