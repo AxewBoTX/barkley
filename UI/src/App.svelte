136638f6950dc48bb9c30ecdf1a17441e16233e9
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-21 20:29:59 +0530

    basic UI

--- UI/src/App.svelte
+++ UI/src/App.svelte
@@ -1,16 +1,52 @@
 <script>
-  import CreateTodoModal from "./components/createTodoModal.svelte";
+  import toast, { Toaster } from "svelte-french-toast";
   import Icon from "@iconify/svelte";
+
+  const fetchTodos = async () => {
+    const res = await fetch("/api/todo");
+    const data = await res.json();
+    return data;
+  };
 </script>
 
-<div class="flex flex-col items-center mt-[50px]">
-  <h1 class="text-4xl">Dondu</h1>
-  <div>
-    <button
-      class="btn"
-      on:click={() => document.getElementById("createTodoModal").showModal()}
-      >CREATE <Icon icon="material-symbols:add" class="text-xl" /></button
+<div class="flex flex-col items-center mt-[50px] px-4">
+  <h1 class="text-5xl mb-[45px]">Dondu</h1>
+  <!-- Create Todo Form -->
+  <form
+    class="join w-full max-w-[800px] flex items-center justify-end"
+    on:submit|preventDefault={(e) => {
+      const form_data = new FormData(e.currentTarget);
+      const formData = {};
+      for (let i of form_data) {
+        const [key, value] = i;
+        formData[key] = value;
+      }
+      if (formData.title.trim().length == 0 || formData.title.trim() == "") {
+        toast.error("Input is needed!", {
+          position: "top-right",
+        });
+      }
+      // ------> Handle Data Posting
+    }}
+  >
+    <input
+      name="title"
+      class="input input-bordered join-item w-full max-w-[200px]"
+      placeholder="Title"
+    />
+    <button class="btn btn-primary btn)-outline join-item" type="submit"
+      ><Icon icon="material-symbols:add" class="text-2xl" /></button
     >
-    <CreateTodoModal />
+  </form>
+  <!-- Todo List -->
+  <div>
+    {#await fetchTodos()}
+      <div class="text-2xl">Loading...</div>
+    {:then data}
+      <div class="">
+        {data.length}
+      </div>
+    {/await}
   </div>
 </div>
+<Toaster />
